8afeb1677ea40841713aaaac4bf3efd5b3a0962d
Tricksire <tricksire@protonmail.com>
2025-09-09 20:56:30 +0100

    Appearence changes, added two more buttons and an input box

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@ function alertFunction() {
 function confirmFunction() {
     confirm("yes or no")
     }
+function outputFunction() {
+    alert(document.getElementById('textInput').value)
+}
+
+function newRow() {
+    const table = document.getElementById("table").
+    getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(1);
+    cell1.innerHTML = "test";
+    cell2.innerHTML = "test";
+    cell3.innerHTML = "test";
+}
 
 document.getElementById('colorPicker1').addEventListener('input', function() {
     document.getElementById('text1').style.color = this.value;

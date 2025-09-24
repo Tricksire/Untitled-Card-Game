a64c026545d236b47caa1854132f932365f0b5be
Tricksire <tricksire@protonmail.com>
2025-09-24 11:34:57 +0100

    Added color input event listener to for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,25 +24,15 @@ function newRow() {
         cell.appendChild(textarea)
     }
 }
-// Creates color input for each paragraph
+
 const paragraph = document.getElementsByTagName("p")
 const numberOfPara = paragraph.length
 for (let i = 0; i < numberOfPara; i++) {
     const input = document.createElement("input")
     input.type = "color", input.value = "#D3D3D3", input.autocomplete = "off", input.class = "center", input.id = "colorPicker" + i.toString()
-
     paragraph[i].insertAdjacentElement("afterend", input);
+    const text = document.getElementById("text" + i.toString())
+    input.addEventListener("input", function () {
+        text.style.color = this.value;
+    })
 }
-
-// Sets the paragraph text color based on the input
-document.getElementById('colorPicker0').addEventListener('input', function () {
-    document.getElementById('text0').style.color = this.value;
-})
-
-document.getElementById('colorPicker1').addEventListener('input', function () {
-    document.getElementById('text1').style.color = this.value;
-})
-
-document.getElementById('colorPicker2').addEventListener('input', function () {
-    document.getElementById('text2').style.color = this.value;
-})
